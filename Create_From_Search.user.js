eacdbdcf6bb7d7e95709933b744c502e7f34cc8a
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-10-31 18:22:12 -0400

    Add Return button on login.

--- Create_From_Search.user.js
+++ Create_From_Search.user.js
@@ -3,6 +3,7 @@
 // @namespace   https://github.com/mattflaschen
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @include     http://en.wikipedia.org/w/index.php?*search=*
+// @include     http://en.wikipedia.org/w/index.php?title=Special:UserLogin&action=submitlogin&type=login*
 // @version     1
 // ==/UserScript==
 
@@ -36,51 +37,77 @@ function makeButton(text, url, isPrimary)
 	return addButtonCss($btn, isPrimary);
 }
 
+function addSearchButtons()
+{
+	var $doesNotExist = $('.mw-search-createlink');
+	$('i, br', $doesNotExist).remove();
+
+	var page = $('#searchText').val();
+	page = page.charAt(0).toUpperCase() + page.substr(1);
+
+	var $create = $('<div />',
+	{
+		css:
+		{
+			marginLeft: '0.4em'
+		}
+	});
+
+	var $buttons;
+	if(mw.config.get('wgUserName') === null)
+	{
+		$buttons =
+		[
+			makeButton('Log In', $('#pt-login a').attr('href'), true),
+			makeButton('Sign Up', $('#pt-createaccount a').attr('href'), true)
+		];
+	}
+	else
+	{
+		var readUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=';
+		var editUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?action=edit&title=';
+		$buttons =
+		[
+			makeButton('Learn More', readUrl + 'Wikipedia:Your_first_article', true),
+			makeButton('Create Draft', editUrl + 'Special:MyPage/Sandbox/' + page, true),
+			makeButton('Create Immediately', editUrl + page, false)
+		];
+	}
+
+	$create.append($('<p />',
+	{
+		text: 'You can create the "' + page + '" article:',
+		css:
+		{
+			fontSize: '135%',
+			fontWeight: 'bold',
+			marginLeft: '0',
+			marginBottom: '0.2em'
+		}
+	}), $buttons);
+
+	$doesNotExist.append($create);
+}
+
+function addReturnButton()
+{
+	var $returnTo = $('#mw-returnto');
+	var html = $returnTo.html();
+	var url = $('a', $returnTo).attr('href');
+	var $btn = makeButton('Return', url, true);
+	html = html.replace('Return', '');
+	$returnTo.html(html).prepend($btn);
+}
+
 var mw = unsafeWindow.mw; // prototype-only
 
-var $doesNotExist = $('.mw-search-createlink');
-$('i, br', $doesNotExist).remove();
-
-var page = $('#searchText').val();
-page = page.charAt(0).toUpperCase() + page.substr(1);
-
-var $create = $('<div />',
-	       {
-		       css:
-		       {
-			       marginLeft: '0.4em'
-		       }
-	       });
-var $buttons;
-if(mw.config.get('wgUserName') === null)
+var pageName = mw.config.get('wgPageName');
+if(pageName == 'Special:UserLogin')
 {
-	$buttons =
-	[
-		makeButton('Log In', $('#pt-login a').attr('href'), true),
-		makeButton('Sign Up', $('#pt-createaccount a').attr('href'), true)
-	];
+	addReturnButton();
 }
-else
+else if(pageName == 'Special:Search')
 {
-	var readUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=';
-	var editUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?action=edit&title=';
-	$buttons =
-	[
-		makeButton('Learn More', readUrl + 'Wikipedia:Your_first_article', true),
-		makeButton('Create Draft', editUrl + 'Special:MyPage/Sandbox/' + page, true),
-		makeButton('Create Immediately', editUrl + page, false)
-	];
+	addSearchButtons();
 }
 
-$create.append($('<p />',
-		       {
-			       text: 'You can create the "' + page + '" article:',
-			       css:
-			       {
-				       fontSize: '135%',
-				       fontWeight: 'bold',
-				       marginLeft: '0',
-				       marginBottom: '0.2em'
-			       }
-		       }), $buttons);
-$doesNotExist.append($create);

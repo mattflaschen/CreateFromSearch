25fbbde90d94e2a9b51bc428e3f097e9ac8333e3
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-10-30 22:10:51 -0400

    Fix login check, add three buttons once they're logged in.

--- Create_From_Search.user.js
+++ Create_From_Search.user.js
@@ -2,32 +2,39 @@
 // @name        Create From Search
 // @namespace   https://github.com/mattflaschen
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
-// @include     http://en.wikipedia.org/w/index.php?title=Special%3ASearch*&search=*
-// @include     http://en.wikipedia.org/w/index.php?title=Special:Search*&search=*
+// @include     http://en.wikipedia.org/w/index.php?*search=*
 // @version     1
 // ==/UserScript==
 
-function makeButton(text, click)
+function makeButton(text, url, isPrimary)
 {
 	// Production version would use classes and separate CSS file.
 	return $('<button />',
 	       {
 		       text: text,
-		       click: click,
+		       click: function()
+		       {
+			       window.location.href = url;
+		       },
 		       css:
 		       {
-			       backgroundColor: 'blue',
-			       color: 'white',
+			       backgroundColor: isPrimary ? 'blue' : '#C9FFF6',
+			       color: isPrimary ? 'white' : 'black',
 			       width: '200px',
 			       height: '40px',
-			       fontWeight: 'bold'
+			       fontWeight: 'bold',
+			       marginRight: '5px'
 		       }
 	       });
 }
 
+var mw = unsafeWindow.mw; // prototype-only
+
 var $doesNotExist = $('.mw-search-createlink');
 $('i, br', $doesNotExist).remove();
+
 var page = $('#searchText').val();
+page = page.charAt(0).toUpperCase() + page.substr(1);
 
 var $create = $('<div />',
 	       {
@@ -36,14 +43,23 @@ var $create = $('<div />',
 			       marginLeft: '0.4em'
 		       }
 	       });
-var $button;
-if(unsafeWindow.mw.config.get('wgUsername') === null)
+var $buttons;
+if(mw.config.get('wgUserName') === null)
 {
-		$button = makeButton('Log In', function()
-                {
-			window.location.href = $('#pt-login a').attr('href');
-		});
+	$buttons = [makeButton('Log In', $('#pt-login a').attr('href'), true)];
 }
+else
+{
+	var readUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=';
+	var editUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?action=edit&title=';
+	$buttons =
+	[
+		makeButton('Learn More', readUrl + 'Wikipedia:Your_first_article', true),
+		makeButton('Create Draft', editUrl + 'Special:MyPage/Sandbox/' + page, true),
+		makeButton('Create Immediately', editUrl + page, false)
+	];
+}
+
 $create.append($('<p />',
 		       {
 			       text: 'To create the "' + page + '" article:',
@@ -54,5 +70,5 @@ $create.append($('<p />',
 				       marginLeft: '0',
 				       marginBottom: '0.2em'
 			       }
-		       }), $button);
+		       }), $buttons);
 $doesNotExist.append($create);
